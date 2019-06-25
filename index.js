c73d7dba3be0263e21fa54e7354516803060830e
xLaki <brandonbedard12@gmail.com>
2019-06-25 11:07:43 -0400

    another package thing because primalgaming doesnt work xddddddd

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const lackage = require('lackage');
 const krazymonkey45 = require('krazymonkey45');
 const primalgaming = require('primalgaming');
-const 
+const crunchyholymonkey = require('crunchyholymonkey');
 
 lackage.laki();
 krazymonkey45.helloWorld();
 primalgaming.helloWorld();
+crunchyholymonkey.byebyeworld();
