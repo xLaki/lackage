9d54cd58cf1afc579e9842d34301b5f4bb29f517
xLaki <brandonbedard12@gmail.com>
2019-06-25 11:06:12 -0400

    everything the just happened in this package thing

---
+++ index.js
@@ -0,0 +1,8 @@
+const lackage = require('lackage');
+const krazymonkey45 = require('krazymonkey45');
+const primalgaming = require('primalgaming');
+const 
+
+lackage.laki();
+krazymonkey45.helloWorld();
+primalgaming.helloWorld();
